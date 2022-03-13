67749629d92f76ff3336989585ec0fa58dddb61a
Hidayat Eko Saputro <hidayat.e.s@mail.ugm.ac.id>
2022-03-13 22:01:46 +0700

    postgres database implemented

---
+++ src/utils/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable camelcase */
+/* eslint linebreak-style: ["error", "windows"] */
+
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  create_at,
+  update_at,
+}) => ({
+  id,
+  title,
+  body,
+  tags,
+  createAt: create_at,
+  updateAt: update_at,
+});
+
+module.exports = { mapDBToModel };
