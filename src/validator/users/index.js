b010d1c99b8331f710ce5f8ab5367bbbdd2f3c8b
Hidayat Eko Saputro <hidayat.e.s@mail.ugm.ac.id>
2022-03-27 12:17:18 +0700

    user registration implemented

---
+++ src/validator/users/index.js
@@ -0,0 +1,16 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+const InvariantError = require('../../exceptions/InvariantError');
+const { UserPayloadSchema } = require('./schema');
+
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validateResult = UserPayloadSchema.validate(payload);
+
+    if (validateResult.error) {
+      throw new InvariantError(validateResult.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator;
