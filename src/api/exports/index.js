b0c9293df37e2e4ae16c5411512d04f70767f3b8
Hidayat Eko Saputro <hidayat.e.s@mail.ugm.ac.id>
2022-04-20 15:56:53 +0700

    export notes implemented

---
+++ src/api/exports/index.js
@@ -0,0 +1,13 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+const ExportsHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'exports',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const exportsHandler = new ExportsHandler(service, validator);
+    server.route(routes(exportsHandler));
+  },
+};
